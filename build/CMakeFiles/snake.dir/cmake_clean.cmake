672a93c0ee84b11b421606959567859aeda44611
Carlos Aguilera <18aguilerac@gmail.com>
2022-03-14 16:50:13 -0700

    snake game init

---
+++ build/CMakeFiles/snake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake.dir/src/main.cpp.o"
+  "CMakeFiles/snake.dir/src/main.cpp.o.d"
+  "snake"
+  "snake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
