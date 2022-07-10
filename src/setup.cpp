1a9aeb5cf82c31e9e47012bdb838e38d444dd34c
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-09 21:35:54 -0700

    fixing folder structure

--- src/setup.cpp
+++ src/setup.cpp
@@ -1,20 +1,19 @@
 #include "../include/header.h"
 
 void setup() {
-   tailx.clear();
-   taily.clear();
+  tailx.clear();
+  taily.clear();
 
-   gameOver = false;
+  gameOver = false;
 
-   playerX = WIDTH / 2;
-   playerY = HEIGHT / 2;
+  playerX = WIDTH / 2;
+  playerY = HEIGHT / 2;
 
-   fruitX = rand() % WIDTH;
-   fruitY = rand() % HEIGHT;
-   
-   tailx.push_back(playerX);
-   taily.push_back(playerY);
+  fruitX = rand() % WIDTH;
+  fruitY = rand() % HEIGHT;
 
-   nTail = 0;
+  tailx.push_back(playerX);
+  taily.push_back(playerY);
 
+  nTail = 0;
 }
